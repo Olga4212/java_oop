5b2241e5cee2024385b5bc592165437411219313
Olga <fenex@bk.ru>
2020-06-03 14:08:49 +0300

    lesson4 code added

---
+++ src/test/java/ru/geekbrains/main/site/at/junit/example/annotaions/DisplayNameTest.java
@@ -0,0 +1,25 @@
+package ru.geekbrains.main.site.at.junit.example.annotaions;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@DisplayName("Простой пример Тестового набора")
+public class DisplayNameTest {
+
+    @DisplayName("Простой пример ТК 1")
+    @Test
+    void example1() {
+        assertTrue(true);
+        assertEquals(2, 1 + 1);
+    }
+
+    @DisplayName("Простой пример ТК 2")
+    @Test
+    void example2() {
+        assertTrue(true);
+        assertEquals(2, 1 + 1);
+    }
+}
